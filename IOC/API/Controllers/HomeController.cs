7ba5b2a45194280f39f96c58f00fb3217022b921
pudding.yang <pudding.yang@ecidh.com>
2019-03-20 17:43:00 +0800

    迁入项目

---
+++ IOC/API/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class HomeController : ApiController
+    {
+
+        public HttpResponseMessage Get()
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+            response.Content = new StringContent("ok123", Encoding.UTF8, string.Format("application/{0}", "json"));
+            response.StatusCode = HttpStatusCode.OK;
+            return response;
+        }
+    }
+}
